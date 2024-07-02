9e00b84e091a5acca2cfa739f01ad8bccef79495
David <witam@tuta.io>
2024-07-02 20:37:40 +0200

    Basic chat usage in page

--- Cwg.WebGui/Program.cs
+++ Cwg.WebGui/Program.cs
@@ -1,8 +1,13 @@
+using Cwg.Core.Services;
+using Cwg.Infrastructure.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Cwg.WebGui;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddTransient<IChatService, ChatService>();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
