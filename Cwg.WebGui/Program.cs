27db53364b846234047ef72538ff4a687951c1db
David <witam@tuta.io>
2024-07-05 19:53:37 +0200

    Refactor: unused parameter

--- Cwg.WebGui/Program.cs
+++ Cwg.WebGui/Program.cs
@@ -15,6 +15,6 @@ builder.Services.AddBlazoredLocalStorage();
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
