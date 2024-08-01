d38928b12b5f8621f052f289076ca6e4535630d3
David <witam@tuta.io>
2024-08-01 20:54:47 +0200

    Chat list

--- Cwg.Core/Dto/ChatInfo.cs
+++ Cwg.Core/Dto/ChatInfo.cs
@@ -2,6 +2,6 @@ namespace Cwg.Core.Dto;
 
 public class ChatInfo
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Title { get; set; } = "New chat";
 }
