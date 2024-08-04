7a110e029f1665eca1e7f8605dc1cca622007bb9
David <witam@tuta.io>
2024-08-04 16:47:46 +0200

    Chat completion implementation

--- Cwg.Core/Services/IChatService.cs
+++ Cwg.Core/Services/IChatService.cs
@@ -1,7 +1,10 @@
+using Cwg.Core.Dto;
+
 namespace Cwg.Core.Services;
 
 public interface IChatService
 {
+    Task CompleteChatAsync(ChatSessionDto chatSession);
     void Initialize(string apiKey, string model = "gpt-4o");
     Task<string> SendAsync(string msg);
 }
--- Cwg.Infrastructure/Services/ChatService.cs
+++ Cwg.Infrastructure/Services/ChatService.cs
@@ -1,3 +1,4 @@
+using Cwg.Core.Dto;
 using Cwg.Core.Services;
 using OpenAI.Chat;
 
@@ -6,6 +7,26 @@ namespace Cwg.Infrastructure.Services;
 public class ChatService : IChatService
 {
     private ChatClient? _chat;
+
+    public async Task CompleteChatAsync(ChatSessionDto chatSession)
+    {
+        IEnumerable<ChatMessage> messages = chatSession.Messages.Select(x =>
+            x.Type == ChatMessageType.Assistant ? new AssistantChatMessage(x.Message) as ChatMessage : new UserChatMessage(x.Message)
+        );
+
+        if (_chat == null)
+            throw new InvalidOperationException();
+        
+        ChatCompletion completion = await _chat.CompleteChatAsync(messages);
+
+        string? message = completion.ToString();
+        
+        chatSession.Messages.Add(new ChatMessageDto
+        {
+            Message = message,
+            Type = ChatMessageType.Assistant
+        });
+    }
 
     public void Initialize(string apiKey, string model = "gpt-4o")
     {
